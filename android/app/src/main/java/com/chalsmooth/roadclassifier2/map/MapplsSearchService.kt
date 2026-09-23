b79bac217a3af6e4c2b3c77225398435b63cfcd0
krish120907 <krishmaurya7156@gmail.com>
2026-09-23 16:59:56 +0000

    Fix polyline decoding coordinates swap, untrack .gradle files, ignore mappls secrets

--- android/app/src/main/java/com/chalsmooth/roadclassifier2/map/MapplsSearchService.kt
+++ android/app/src/main/java/com/chalsmooth/roadclassifier2/map/MapplsSearchService.kt
@@ -149,38 +149,8 @@ class MapplsSearchService private constructor() {
     }
 
     private fun parsePolylineGeometry(polylineStr: String): List<LatLng> {
-        val poly = ArrayList<LatLng>()
-        var index = 0
-        val len = polylineStr.length
-        var lat = 0
-        var lng = 0
-
-        while (index < len) {
-            var b: Int
-            var shift = 0
-            var result = 0
-            do {
-                b = polylineStr[index++].code - 63
-                result = result or (b and 0x1f shl shift)
-                shift += 5
-            } while (b >= 0x20)
-            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
-            lat += dlat
-
-            shift = 0
-            result = 0
-            do {
-                b = polylineStr[index++].code - 63
-                result = result or (b and 0x1f shl shift)
-                shift += 5
-            } while (b >= 0x20)
-            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
-            lng += dlng
-
-            val p = LatLng(lat.toDouble() / 1E5, lng.toDouble() / 1E5)
-            poly.add(p)
-        }
-        return poly
+        return com.mappls.sdk.geojson.utils.PolylineUtils.decode(polylineStr, 6)
+            .map { LatLng(it.latitude(), it.longitude()) }
     }
 
     companion object {
